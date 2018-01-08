db1624a68dc107c3690905ddbd1f47af54182dd0
Brian Boucher <bboucher@maxeler.com>
2018-01-08 13:12:50 -0600

    Adding GPIOPin class

---
+++ inc/GPIOPin.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+class GPIOPin {
+public:
+    enum class Direction {
+        INPUT,
+        OUTPUT
+    };
+    GPIOPin(unsigned char pin, Direction dir = Direction::OUTPUT);
+    GPIOPin(const GPIOPin& rhs) = delete;
+    GPIOPin(GPIOPin&& rhs) = delete;
+    ~GPIOPin();
+
+    void setDirection(Direction dir);
+    void setValue(bool value);
+    bool getValue();
+
+private:
+    const unsigned char pin;
+    Direction dir;
+};
---
+++ src/GPIOPin.cpp
@@ -0,0 +1,92 @@
+#include <GPIOPin.hpp>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::ofstream;
+using std::ifstream;
+using std::stringstream;
+
+namespace {
+    constexpr size_t MAX_GPIO_PIN = 27;
+
+    vector<bool>& activePins() {
+        static vector<bool> pins(MAX_GPIO_PIN + 1, false);
+        return pins;
+    }
+
+    const string GPIO_EXPORT_PATH{ "/sys/class/gpio/export" };
+    void exportPin(size_t pin) {
+        auto& active = activePins();
+        if (active[pin]) {
+            std::cerr << "Cannot export pin " << pin
+                      << " - already active" << std::endl;
+        } else {
+            ofstream fout{ GPIO_EXPORT_PATH };
+            fout << pin; active[pin] = true;
+        }
+    }
+
+    const string GPIO_UNEXPORT_PATH{ "/sys/class/gpio/unexport" };
+    void unexportPin(size_t pin) {
+        auto& active = activePins();
+        if (!active[pin]) {
+            std::cerr << "Cannot unexport pin " << pin
+                << " - not active" << std::endl;
+        } else {
+            ofstream fout{ GPIO_UNEXPORT_PATH };
+            fout << pin; active[pin] = false;
+        }
+    }
+
+    const string GPIO_ROOT{ "/sys/class/gpio/gpio" };
+    const string s_direction{ "direction" };
+    const string s_value{ "value" };
+    string gpioPath(size_t pin, const string& file) {
+        stringstream ss;
+        ss << GPIO_ROOT << pin << '/' << file;
+        return ss.str();
+    }
+}
+
+GPIOPin::GPIOPin(unsigned char pin, Direction dir) : pin(pin), dir(dir) {
+    exportPin(pin);
+    setDirection(dir);
+}
+
+GPIOPin::~GPIOPin() {
+    unexportPin(pin);
+}
+
+void GPIOPin::setDirection(GPIOPin::Direction dir) {
+    ofstream fout{ gpioPath(pin, s_direction) };
+    switch (dir) {
+        case GPIOPin::Direction::INPUT: fout << "in"; break;
+        case GPIOPin::Direction::OUTPUT: fout << "out"; break;
+    }
+}
+
+void GPIOPin::setValue(bool value) {
+    if (dir != GPIOPin::DIRECTION::OUTPUT) {
+        std::cerr << "Cannot set value for pin " << pin
+                  << " - direction is set to INPUT" << std::endl;
+    } else {
+        ofstream fout{ gpioPath(pin, s_value) };
+        fout << (value ? 1 : 0);
+    }
+}
+
+bool GPIOPin::getValue() {
+    if (dir != GPIOPin::DIRECTION::INPUT) {
+        std::cerr << "Cannot get value for pin " << pin
+                  << " - direction is set to OUTPUT" << std::endl;
+    } else {
+        size_t value;
+        ifstream fin{ gpioPath(pin, s_value) };
+        fin >> value;
+        return (value != 0);
+    }
+}
